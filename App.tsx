6ae0511ac2a86282273c9ccfda18f68c61949083
Imran Hossain <imranoffice28@gmail.com>
2025-07-31 11:37:02 +0600

    Add theming

--- App.tsx
+++ App.tsx
@@ -1,8 +1,25 @@
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import Main from "./src/_layout/Main";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { ThemeProvider } from "./src/theme/ThemeProvider";
+import { StyleSheet } from "react-native";
 
 export default function App() {
   return (
-    <Main/>
+     <GestureHandlerRootView style={styles.root}>
+
+       <SafeAreaProvider>
+
+       <ThemeProvider>
+
+       <Main/>
+       </ThemeProvider>
+       </SafeAreaProvider>
+     </GestureHandlerRootView>
   );
 }
 
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },})
--- src/_layout/Main.tsx
+++ src/_layout/Main.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import AppNavigator from '../navigation/AppNavigator'
+import { SafeAreaView, StatusBar } from 'react-native'
+import { useThemeContext } from '../theme/ThemeProvider'
 
 const Main = () => {
+  const {theme}=useThemeContext()
   return (
-   <AppNavigator/>
+    <SafeAreaView  style={{flex:1, }}>
+       <StatusBar
+        translucent
+        backgroundColor="transparent"
+        barStyle={theme === "dark" ? "light-content" : "dark-content"}
+      />
+
+      <AppNavigator/>
+    </SafeAreaView>
   )
 }
 
